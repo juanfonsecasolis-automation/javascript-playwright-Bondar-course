9c00d9ce1537d11ffc80bbdd456dfa3a9e9fd528
Juan Fonseca <juan.fonsecasolis@outlook.com>
2025-01-20 13:20:12 -0600

    Added lesson 6 of functions.

---
+++ helper/printHelper.js
@@ -0,0 +1,5 @@
+export function printAge(age)
+// specify 'export' keyword so function can be used outside the scope of this file
+{
+    console.log(`Age is ${age}.`)
+}
---
+++ lessons/lesson8.js
@@ -0,0 +1,41 @@
+// Declarative functions
+helloOne()
+function helloOne()
+{
+    console.log('Hello one!')
+}
+helloOne()
+
+// Anonymous function
+// helloTwo() // this won't work (function declaration must go first)
+var helloTwo = function(){
+    console.log('Hello two!')
+}
+helloTwo()  
+
+// ES6 function
+var helloThree = () => {
+    console.log('Hello three!')
+}
+helloThree()
+
+// function with arguments
+function printName(firstName, lastName)
+{
+    console.log(`Hello ${firstName} ${lastName}!`)
+}
+printName('Juan', 'Fonseca')
+
+// function with return
+function multiplyByTwo(number)
+{
+    return 2*number
+}
+console.log(multiplyByTwo(3))
+
+// importing functions
+import {printAge} from '../helper/printHelper.js'
+printAge(5)
+
+import * as helper from '../helper/printHelper.js'
+helper.printAge(5)
