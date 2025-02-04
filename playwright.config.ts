bb85419faa82ed989395702e34e8358acd623dab
Juan Fonseca <juan.fonsecasolis@outlook.com>
2025-02-03 18:17:51 -0600

    Added code for timeouts.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  //globalTimeout: 68000,
   //timeout: 20000,
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -31,6 +32,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    //actionTimeout: 5000,
+    //navigationTimeout: 5000
   },
 
   /* Configure projects for major browsers */
--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -7,12 +7,15 @@
 import {test, expect} from '@playwright/test'
 
 // this applies to all test suites below
-test.beforeEach(async ({page}) => {
+test.beforeEach(async ({page}, testInfo) => {
+    // the button on the page below takes 15s to complete
     await page.goto('http://uitestingplayground.com/ajax')
 
     // auto-wait before performing an action (https://playwright.dev/docs/actionability)
     // locator.click() waits for element to be visible, stable, no pending events, enabled
     await page.locator('button', {hasText:'Button Triggering AJAX Request'}).click()
+
+    testInfo.setTimeout(testInfo.timeout + 2000)
 })
 
 test.describe('Waiting suite', () => {
@@ -50,5 +53,22 @@ test.describe('Waiting suite', () => {
         const message = await page.locator("//*[@class='bg-success']").allTextContents()
         expect(message).toContain("Data loaded with AJAX get request.")
     })
+
+    test('timeout specified at the action', async ({page}) => {
+        const successButton = page.locator(".bg-success")
+        await successButton.click({timeout: 17000}) // overrides timeout in the playwright.config.ts
+    })
+
+    test('timeout specified at the beginning of the test', async ({page}) => {
+        test.setTimeout(17000)  // overrides timeout in the playwright.config.ts
+        const successButton = page.locator(".bg-success")
+        await successButton.click() 
+    })
+
+    test('slow tag', async ({page}) => {
+        test.slow()  // overrides timeout in the playwright.config.ts
+        const successButton = page.locator(".bg-success")
+        await successButton.click() 
+    })    
 
 })
