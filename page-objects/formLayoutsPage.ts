71b8553f9ff7780cfee7edff4257fc744ba69888
Juan Fonseca <juan.fonsecasolis@outlook.com>
2025-02-27 16:06:01 -0600

    Added page method with parameters and example of documentation.

---
+++ page-objects/formLayoutsPage.ts
@@ -0,0 +1,45 @@
+/**
+ * 2025 Juan M. Fonseca-Solís
+ * Playwright: Web Automation Testing From Zero to Hero course by Artem Bondar.
+ */
+
+import {Locator, Page} from '@playwright/test'
+
+export class FormLayoutsPage 
+{
+    readonly page: Page
+
+    constructor(page: Page){
+        this.page = page
+    }
+
+    async submitUsingTheGridFormWithCredentialsAndSelectOption(
+        email:string, password:string, optionText:string)
+    {
+        const usingTheGridForm = this.page.locator('nb-card', {hasText: "Using the Grid"});
+        await usingTheGridForm.getByRole('textbox', {name: 'Email'}).fill(email)
+        await usingTheGridForm.getByRole('textbox', {name: 'Password'}).fill(password)
+        await usingTheGridForm.getByRole('radio', {name: optionText}).check({force: true})
+        await usingTheGridForm.getByRole('button').click()
+    }
+
+    /**
+     * Fills the inline form with the name, email, and remember me checkbox
+     * @param name - should be first and last name
+     * @param email - follow the format name@domain.extension
+     * @param rememberMe - true or false if session to be remembered
+     */
+    async submitInlineFormWithEmailEmailAndCheckbox(
+        name:string, email:string, rememberMe:boolean)
+    {
+        const inlineForm = this.page.locator('nb-card', {hasText: "Inline form"});
+        await inlineForm.getByRole('textbox', {name: 'Jane Doe'}).fill(name)
+        await inlineForm.getByRole('textbox', {name: 'Email'}).fill(email)
+        
+        if(rememberMe)
+        {
+            await inlineForm.getByRole('checkbox').check({force: true})
+        }
+        await inlineForm.getByRole('button').click()
+    }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -3,9 +3,10 @@
  * Playwright: Web Automation Testing From Zero to Hero course by Artem Bondar.
  */
 
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { assert } from 'console'
-import {NavigationPage} from '../page-objects/navigationPage'
+import { NavigationPage } from '../page-objects/navigationPage'
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
@@ -18,4 +19,20 @@ test('navigate to form page', async ({page}) => {
     await navigateTo.smartTablePage();
     await navigateTo.toastrPage();
     await navigateTo.tooltipPage();
+})
+
+test('parameterized methods', async ({page}) => {
+    const navigateTo = new NavigationPage(page)
+    const onFormLayoutPage = new FormLayoutsPage(page)
+    await navigateTo.formLayoutsPage()
+    await onFormLayoutPage.submitUsingTheGridFormWithCredentialsAndSelectOption(
+        'test@test.com',
+        'Welcome1',
+        'Option 2'
+    )
+    await onFormLayoutPage.submitInlineFormWithEmailEmailAndCheckbox(
+        'John Smith',
+        'john@test.com',
+        true
+    )
 })
